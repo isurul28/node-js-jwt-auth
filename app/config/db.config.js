be355063cd62d24224c8ff7ae6713b0e5032981e
Isuru Liyanage <isuruliyanage1@gmail.com>
2023-12-04 12:42:00 +0530

    Update db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  HOST: "mysql-44a3265f-038c-4f64-b03f-a5487af6389e-mysqldb4054402929-ch.a.aivencloud.com",
-  USER: "avnadmin",
-  PORT: "21955",
-  PASSWORD: "AVNS_Lu_Hj2U3xD8b4ncEtbN",
+  HOST: "xxxx",
+  USER: "xxx",
+  PORT: "xxxx",
+  PASSWORD: "xxxx",
   DB: "authdb",
   dialect: "mysql",
   pool: {
