7bebcf8e4772960a3fda98ea3ea5e63d107bdc53
isurul <isuruliyanage1@gmail.com>
2023-12-04 15:05:28 +0530

    added new config

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  HOST: "xxxx",
-  USER: "xxx",
-  PORT: "xxxx",
-  PASSWORD: "xxxx",
-  DB: "authdb",
+  HOST: "process.env.APP_SERVER",
+  USER: "process.env.APP_USERNAME",
+  PORT: "process.env.APP_PORT",
+  PASSWORD: "process.env.APP_PASSWORD",
+  DB: "process.env.APP_DATABASE",
   dialect: "mysql",
   pool: {
     max: 5,
--- app/config/dbcrud.config.js
+++ app/config/dbcrud.config.js
@@ -1,10 +1,14 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+
 const mysql = require('mysql')
 const cruddb = mysql.createConnection({
-host: "xxxx",
-port:"xxx",
-user: "xxx",
-password: "xxxxx",
-database:"crudDB" 
+host: "process.env.APP_SERVER",
+port:"process.env.APP_PORT",
+user: "process.env.APP_USERNAME",
+password: "process.env.APP_PASSWORD",
+database:"process.env.APP_DATABASE" 
 })
 
 module.exports = cruddb;
