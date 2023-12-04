61dd10ece4bd1c08db0d732839775e33b7c11966
Isuru Liyanage <isuruliyanage1@gmail.com>
2023-12-04 12:42:30 +0530

    Update dbcrud.config.js

--- app/config/dbcrud.config.js
+++ app/config/dbcrud.config.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql')
 const cruddb = mysql.createConnection({
-host: "mysql-44a3265f-038c-4f64-b03f-a5487af6389e-mysqldb4054402929-ch.a.aivencloud.com",
-port:"21955",
-user: "avnadmin",
-password: "AVNS_Lu_Hj2U3xD8b4ncEtbN",
+host: "xxxx",
+port:"xxx",
+user: "xxx",
+password: "xxxxx",
 database:"crudDB" 
 })
 
